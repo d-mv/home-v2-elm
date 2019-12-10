88321788f3a6f2242d8aa7365d7a440842c5b2fa
d-mv <d.dash.mv@gmail.com>
2019-12-10 09:24:53 +0200

    style: setup styles, global, button

--- src/Main.elm
+++ src/Main.elm
@@ -1,1 +1,32 @@
 module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+
+-- main : Program () Model Msg
+
+
+isActive =
+    True
+
+
+main =
+    div []
+        [ button
+            [ classList [ ( "nav-button", True ), ( "active", isActive ) ]
+            ]
+            [ text "Hello World" ]
+        ]
+
+
+
+-- Browser.application
+--     { init = init
+--     , view = view
+--     , update = update
+--     , subscriptions = subscriptions
+--     , onUrlChange = onUrlChange
+--     , onUrlRequest = onUrlRequest
+--     }
